edd7783254730706871a883f1128b38ad91cac3d
Ahamed Basith <81899645+alahamedbasith@users.noreply.github.com>
2022-10-29 22:26:40 +0530

    Create input_output.c

---
+++ Assignment_01/C_Assignment/input_output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{ 
+ int number = 5; 
+ printf("Number = %d", number); 
+
+ float number1 = 13.5; 
+ double number2 = 12.4; 
+ printf("number1 = %f\n", number1); 
+ printf("number2 = %lf", number2); 
+
+ char chr = 'a'; 
+ printf("character = %c", chr); 
+ 
+ return 0; 
+} 
