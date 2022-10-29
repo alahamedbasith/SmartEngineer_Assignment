5ff530171f47cb1c595011b14716425b9e4b6fcf
Ahamed Basith <81899645+alahamedbasith@users.noreply.github.com>
2022-10-29 23:50:18 +0530

    Update area_circle.java

--- Assignment_01/Java_Assignment/area_circle.java
+++ Assignment_01/Java_Assignment/area_circle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AreaOfCircle {
+public class area_circle {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Enter the radius: ");
